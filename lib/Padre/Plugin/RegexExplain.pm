50efc16c06782e26e7a3d3dff07a6af675310175
reneeb <github@renee-baecker.de>
2010-09-08 14:23:06 +0200

    switch to Dist::Zilla

--- lib/Padre/Plugin/RegexExplain.pm
+++ lib/Padre/Plugin/RegexExplain.pm
@@ -81,14 +81,9 @@ sub explain {
 }
 
 
+1; # End of Padre::Plugin::RegexExplain
 
-=head1 NAME
-
-Padre::Plugin::RegexExplain - The great new Padre::Plugin::RegexExplain!
-
-=head1 VERSION
-
-Version 0.01
+# ABSTRACT: A Padre plugin for Regex explainations
 
 =head1 SYNOPSIS
 
@@ -101,61 +96,4 @@ Perhaps a little code snippet.
     my $foo = Padre::Plugin::RegexExplain->new();
     ...
 
-=head1 AUTHOR
 
-Renee Baecker, C<< <module at renee-baecker.de> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-padre::plugin::regexexplain at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Padre::Plugin::RegexExplain>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Padre::Plugin::RegexExplain
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Padre::Plugin::RegexExplain>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Padre::Plugin::RegexExplain>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Padre::Plugin::RegexExplain>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Padre::Plugin::RegexExplain/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2010 Renee Baecker.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
-
-1; # End of Padre::Plugin::RegexExplain
